638d77db8b712996ad7236ab8d4056e0c6fa025a
Re-Kor <chyun1750@gmail.com>
2022-10-21 10:11:37 +0900

    update

--- src/components/ContextMenu.js
+++ src/components/ContextMenu.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Menu, Item, Separator, Submenu, contextMenu } from 'react-contexify';
-import 'react-contexify/dist/ReactContexify.min.css';
+import React from "react";
+import { Menu, Item, Separator, Submenu, contextMenu } from "react-contexify";
+import "react-contexify/dist/ReactContexify.min.css";
 
 export { contextMenu };
 
@@ -11,11 +11,11 @@ export default class extends React.Component {
       loading: [],
       contextMenuItems: [
         {
-          title: '导出',
-          command: '0',
+          title: "导出",
+          command: "0",
           children: [
-            { title: '图片', command: '01' },
-            { title: 'PDF', command: '02' },
+            { title: "图片", command: "01" },
+            { title: "PDF", command: "02" },
           ],
         },
       ],
@@ -23,43 +23,43 @@ export default class extends React.Component {
   }
 
   render() {
-    return (
-      <Menu id={this.props.id} animation="pop" theme="light">
-        {this.state.contextMenuItems.map((item, index) => {
-          if (item.children && item.children.length) {
-            return (
-              <Submenu label={item.title}>
-                {item.children.map((el, ind) => (
-                  <Item
-                    key={ind}
-                    onClick={() => {
-                      if (item.command === '0') {
-                        this.props.loading();
-                        setTimeout(() => {
-                          this.props.stopLoading();
-                        }, 1000);
-                      }
-                      this.props.onClick(el);
-                    }}
-                  >
-                    {el.title}
-                  </Item>
-                ))}
-              </Submenu>
-            );
-          }
-          return (
-            <Item
-              key={index}
-              onClick={() => {
-                this.props.onClick(item);
-              }}
-            >
-              {item.title}
-            </Item>
-          );
-        })}
-      </Menu>
-    );
+    // return (
+    //   <Menu id={this.props.id} animation="pop" theme="light">
+    //     {this.state.contextMenuItems.map((item, index) => {
+    //       if (item.children && item.children.length) {
+    //         return (
+    //           <Submenu label={item.title}>
+    //             {item.children.map((el, ind) => (
+    //               <Item
+    //                 key={ind}
+    //                 onClick={() => {
+    //                   if (item.command === '0') {
+    //                     this.props.loading();
+    //                     setTimeout(() => {
+    //                       this.props.stopLoading();
+    //                     }, 1000);
+    //                   }
+    //                   this.props.onClick(el);
+    //                 }}
+    //               >
+    //                 {el.title}
+    //               </Item>
+    //             ))}
+    //           </Submenu>
+    //         );
+    //       }
+    //       return (
+    //         <Item
+    //           key={index}
+    //           onClick={() => {
+    //             this.props.onClick(item);
+    //           }}
+    //         >
+    //           {item.title}
+    //         </Item>
+    //       );
+    //     })}
+    //   </Menu>
+    // );
   }
 }
